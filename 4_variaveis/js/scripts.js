539e25e41ae1a147fc94c12ff8370746c764a409
Juliano <72839366+julianocgc@users.noreply.github.com>
2026-05-19 18:56:47 -0300

    Envio de arquivos
    Envio de arquivos JavaScript.

---
+++ 4_variaveis/js/scripts.js
@@ -0,0 +1,43 @@
+var teste = 1;
+
+var nome = "João";
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'ASD';
+var _nome = 'ASD';
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+varMeuPrimeiroNome = "Juliano";
+
+console.log(varMeuPrimeiroNome); // camelCase
+
+var meusobrenome = "Guimarães";
+
+console.log(meusobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = "Vitor";
+
+console.log(meuNome);
