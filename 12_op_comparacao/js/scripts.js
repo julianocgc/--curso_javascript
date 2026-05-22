132b49208bf5b6a6dc651d86a214d58999934151
Juliano <72839366+julianocgc@users.noreply.github.com>
2026-05-22 11:48:01 -0300

    Enviar arquivos
    Enviar arquivos JavaScript.

---
+++ 12_op_comparacao/js/scripts.js
@@ -0,0 +1,32 @@
+var idade = 15;
+var possuiCarro = 1;
+
+if(idade >= 18) {
+    console.log("O usuário pode tirar sua carteira de motorista!")
+}
+
+if(idade <= 17) {
+    console.log("O usuário não pode fazer a carteira de motorista...")
+}
+
+if(possuiCarro) {
+    console.log("O usuário já pode andar de carro.");
+}
+
+var nome = "Vitor";
+
+if(nome == "Juliano") {
+    console.log("O seu nome é Juliano");
+}
+
+if(nome != "Juliano") {
+    console.log("O seu nome não é Juliano");
+}
+
+if(20 > 10) {
+    console.log("Passou");
+}
+
+if(10 < 20) {
+    console.log("Passou 2");
+}
