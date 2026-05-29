600d9e91e0a53759a157f899d9482231555d66b7
Juliano <72839366+julianocgc@users.noreply.github.com>
2026-05-29 10:44:41 -0300

    Envio de arquivos
    Envio de arquivos JavaScript.

---
+++ 16_operador_or/js/scripts.js
@@ -0,0 +1,14 @@
+var idade = 17;
+var nome = "João";
+
+if(nome == "João" || idade > 14) {
+    console.log("Pode entrar na aula de esgrima.")
+} else {
+    console.log("Não pode entrar.");
+}
+
+if((nome == "Pedro" || 30 > 20) && 10 == 10) {
+    console.log("testando");
+} else {
+    console.log("não entrou")
+}
