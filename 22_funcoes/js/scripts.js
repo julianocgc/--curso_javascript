c232866dbbfe48eb4a80c04e40e78b72297a8076
Juliano <72839366+julianocgc@users.noreply.github.com>
2026-06-05 11:09:26 -0300

    Envio de arquivos
    Envio de arquivos JavaScript.

---
+++ 22_funcoes/js/scripts.js
@@ -0,0 +1,33 @@
+function primeiraFuncao() {
+    console.log("Hello World das Funções");
+}
+
+primeiraFuncao();
+primeiraFuncao();
+primeiraFuncao();
+
+
+function dizerNome(nome) {
+    console.log("O nome é: " + nome);
+}
+
+dizerNome("Matheus");
+dizerNome("Juliano");
+dizerNome("Vitor");
+
+var nomeDoBancoDeDados = "João";
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a, b) {
+    var soma = a + b;
+    return soma;
+}
+
+var somaUm = soma(2,5);
+console.log(somaUm);
+
+var somaDois = soma(5, 5);
+console.log(somaDois);
+
+console.log(soma(4, 5));
