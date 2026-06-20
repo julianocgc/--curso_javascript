eb9b050258a2b30671faece29b94efd64e7a9afc
Juliano <72839366+julianocgc@users.noreply.github.com>
2026-06-20 14:19:32 -0300

    Envio de arquivos
    Envio de arquivos JavaScript.

---
+++ 31_objetos/js/scripts.js
@@ -0,0 +1,18 @@
+let pessoa = {
+    nome: "Matheus",
+    idade: 29,
+    falar: function() {
+        console.log("Olá, tudo bem?");
+    },
+    soma: function(a, b) {
+        return a + b;
+    }
+}
+
+console.log(pessoa.nome);
+
+pessoa.falar();
+
+var soma = pessoa.soma(2, 2);
+
+console.log(soma);
