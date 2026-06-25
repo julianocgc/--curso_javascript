35acd0c43b6eef36174eec497d8ebddc808a9f22
Juliano <72839366+julianocgc@users.noreply.github.com>
2026-06-25 11:26:34 -0300

    Envio de arquivos
    Envio de arquivos JavaScript.

---
+++ 32_this/js/scripts.js
@@ -0,0 +1,42 @@
+console.log(this);
+
+alert('olá');
+this.alert('olá 2');
+
+var teste = 5;
+console.log(this.teste);
+console.log(teste);
+
+let pessoa = {
+    nome: "Matheus",
+    idade: 29,
+    falar: function() {
+        console.log("Olá, tudo bem?");
+    },
+
+    dizerNome: function() {
+        console.log("O meu nome é " + this.nome);
+    },
+
+    aniversario: function() {
+        this.idade += 1;
+    },
+
+    saudacao: function() {
+        return 'Sr. ' + this.nome;
+    }
+};
+
+pessoa.dizerNome();
+
+console.log(pessoa.idade);
+
+pessoa.aniversario();
+pessoa.aniversario();
+pessoa.aniversario();
+
+console.log(pessoa.idade);
+
+var sdc = pessoa.saudacao();
+
+console.log("Olá " + sdc);
